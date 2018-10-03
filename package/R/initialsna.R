8189bca32dee8675f01005f7f41a118fef3731c1
Yuchao Jiang <yuchaojiang@users.noreply.github.com>
2018-10-03 12:10:23 -0400

    Update initialsna.R

--- package/R/initialsna.R
+++ package/R/initialsna.R
@@ -1,6 +1,10 @@
 initialsna = function(tree, sna.name) {
     sna.no = length(sna.name)
-    sna.edge = sample(2:nrow(tree$edge), size = sna.no, replace = TRUE)
+    if(nrow(tree$edge)>2){
+      sna.edge = sample(2:nrow(tree$edge), size = sna.no, replace = TRUE)
+    } else{
+      sna.edge = rep(2, sna.no)
+    }
     sna.mat = cbind(1:sna.no, tree$edge[sna.edge, ])
     colnames(sna.mat) = c("sna", "sna.st.node", "sna.ed.node")
     rownames(sna.mat) = sna.name
